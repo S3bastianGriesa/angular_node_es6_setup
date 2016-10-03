6343189785692760f0a7f0ee8da6dde04242cbd7
Sebastian Griesa <sebastian.griesa@gmail.com>
2016-10-03 12:56:31 +0200

    Add startsite

---
+++ public/js/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('app', ['ngResource', 'ngRoute', 'ui.bootstrap', 'ngAnimate'])
+  .config([
+    '$routeProvider', ($routeProvider) => {
+      $routeProvider.when('/', {
+        templateUrl: './js/directives/startsite/template.html',
+        controller: 'startsiteCtrl'
+      })
+    }
+  ]);
